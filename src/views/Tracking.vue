f405d2e9c60177dad565e4a4fc469a11378d3eb6
Achilles45 <achillesusuoyibo7@gmail.com>
2020-02-23 09:09:14 -0800

    Modified the result page

--- src/views/Tracking.vue
+++ src/views/Tracking.vue
@@ -2,16 +2,10 @@
   <div class="track">
     <Topbar />
     <Navbar />
-    <div class="track__heading">
-      <div class="container">
-        <h2>Track Your Items</h2>
-        <p>Get real time information on the state of your shipments</p>
-      </div>
-    </div>
     <div class="container">
       <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 content__holder">
           <form action="" @submit.prevent="track(trackingId)">
             <div class="form-group">
               <div class="text">
@@ -28,64 +22,62 @@
               </button>
             </div>
           </form>
-        </div>
-        <div class="col-md-3"></div>
-      </div>
-      <div class="row" v-if="foundTrackedPackage">
-        <div class="col-md-12">
-         <img src="../assets/images/barcode.webp" class="icon" alt="">
-          {{trackedPackage.id}}<br /><br /><br />
-          <h6>Thank you for trusting us to deliver your package</h6>
-          <small>These is the information about your order</small>
-          <hr>
-          <br>
-        </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-            <h5>Package Detials</h5>
-            <small>These refers to the information about the package in transit. Please contact us if you feel it's not correct</small><br /><br>
-          <p>Package ID: {{trackedPackage.id}}</p>
-          <p>Package Name: {{trackedPackage.name}}</p>
-          <p>Package Price: {{trackedPackage.price}}</p>
-          <p>Date of Purchase: {{trackedPackage.date__of__purchase}}</p>
-          <p>Expected Date Of Delivery: {{trackedPackage.expected__date}}</p>
-          <br>
-        </div>
-         <div class="col-md-6">
-            <h5>Buyers Details</h5>
-            <small>Your information as provided while placing this order</small><br /><br>
-          <p>Buyers Name: {{trackedPackage.buyers__name}}</p>
-          <p>Buyers Name: {{trackedPackage.buyers__email}}</p>
-          <p>Buyers Contact Address: {{trackedPackage.buyers__contact}}</p>
-          </div>
-          </div><br>
-            <div class="row">
-            <div class="col-md-6">
-            <h5>Sellers Detials</h5>
-            <small>The information of the seller while receiving this order</small><br><br>
-          <p>Sellers Name: {{trackedPackage.sellers__name}}</p>
-          <p>Sellers Email: {{trackedPackage.sellers__email}}</p>
-          <p>Sellers Orgaization: {{trackedPackage.sellers__organization}}</p>
-          <br>
-        </div>
-         <div class="col-md-6">
-            <h5>Current State Of Consignment</h5>
-            <small>This is the current state of things about this package</small><br /><br>
-          <p>Current Location: {{trackedPackage.current_location}}</p>
-          <p>Current Country: {{trackedPackage.current__country}}</p>
-          </div>
-          </div>
-      </div>
-    <br /><br />
-
-    <div class="bottom pt-5 pb-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-        <small>The company shall be held responsible for any alteration or physical damage made to your package while with us during transit. Please if you need any clarification on our policies and operation, please send a message in the contact section and a customer support persion will  get back to you in less than 24hours. As at this time, the google maps API is unvailable.</small>
+          <div class="result__wrapper">
+            <table class="table mt-5">
+  <tbody>
+    <tr>
+      <td>PACKAGE ID</td>
+      <td>{{trackedPackage.id}}</td>
+    </tr>
+     <tr>
+      <td>PACKAGE NAME</td>
+      <td>{{trackedPackage.name}}</td>
+    </tr>
+     <tr>
+      <td>PACKAGE PRICE</td>
+      <td>{{trackedPackage.price}}</td>
+    </tr>
+    <tr>
+      <td>DATE OF PURCHASE</td>
+      <td>{{trackedPackage.date__of__purchase}}</td>
+    </tr>
+     <tr>
+      <td>EXPECTED DATE OF DELIVERY</td>
+      <td>{{trackedPackage.expected__date}}</td>
+    </tr>
+    <tr>
+      <td>BUYERS NAME</td>
+      <td>{{trackedPackage.buyers__name}}</td>
+    </tr>
+    <tr>
+      <td>BUYERS EMAIL</td>
+      <td>{{trackedPackage.buyers__email}}</td>
+    </tr>
+    <tr>
+      <td>BUYERS CONTACT ADDRESS</td>
+      <td>{{trackedPackage.buyers__contact}}</td>
+    </tr>
+    <tr>
+      <td>SELLERS NAME</td>
+      <td>{{trackedPackage.sellers__name}}</td>
+    </tr>
+    <tr>
+      <td>SELLERS EMAIL</td>
+      <td>{{trackedPackage.sellers__email}}</td>
+    </tr>
+     <tr>
+      <td>CURRENT LOCATION</td>
+      <td>{{trackedPackage.current_location}}</td>
+    </tr>
+    <tr>
+      <td>COUNTRY</td>
+      <td>{{trackedPackage.current__country}}</td>
+    </tr>
+  </tbody>
+</table>
           </div>
         </div>
+        <div class="col-md-2"></div>
       </div>
     </div>
     <Footer />
@@ -134,7 +126,11 @@ export default {
 
 <style lang="scss" scoped>
 .track {
-  background: #f4f4f4;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url('../assets/images/background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
   .track__heading {
     background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
       url('../assets/images/map.jpeg');
@@ -153,11 +149,25 @@ export default {
       color: #fff;
     }
   }
-  form {
+  .result__holder{
+    background:rgba(0,0,0,.5);
+  }
+ .content__holder{
+   background: rgba(0, 0, 0, 0.5);
+   color: #fff;
+   hr{
+  background:#fff !important;
+   }
+   h5{
+     color:#fff;
+     font-weight: normal;
+     font-size: 1rem;
+   }
+    form {
     margin-top: 5rem;
-    margin-bottom: 5rem;
+    margin-bottom: 3rem;
     label {
-      color: #545454;
+      color: #fff;
       font-size: 0.84rem;
       opacity: 0.9;
       padding-bottom: 0.5rem;
@@ -180,19 +190,11 @@ export default {
       font-size: 0.85rem;
     }
   }
+ }
 }
-h5{
-  color:#454545;
-  font-size: 1.2rem;
-  font-weight: bold;
-}
-p{
-  color:#545454;
+table{
+  color:#fff;
+  opacity: .8;
   font-size: .9rem;
-
-}
-.icon{
-  max-width: 100px;
-  height: auto;
 }
 </style>
